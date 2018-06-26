3e5a4ae733bba00e9d81f6a16686b89377124eed
dibyadarshan hota <dibyadarshan.hota@gmail.com>
2018-06-27 00:33:35 +0530

    Sparse table

---
+++ sparse_table.cpp
@@ -0,0 +1,23 @@
+// http://codeforces.com/problemset/problem/359/D
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define rep(i,a,n) for(long long i=a;i<=n;++i)
+#define dbg1(a,b,c) cout<<"\n->"<<a<<" "<<b<<" "<<c<<"\n"; 
+#define dbg2(a,b,c) cout<<"\n--"<<a<<" "<<b<<" "<<c<<"\n"; 
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+ll mod=(ll)1e9+7;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+
+    
+	return 0;
+}
